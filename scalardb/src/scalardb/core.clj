1e73be983b79abb0deaa873164f2b2db460ef538
yito88 <yuji@phact-columba.com>
2019-06-20 14:06:24 +0900

    refactor scalardb test

--- scalardb/src/scalardb/core.clj
+++ scalardb/src/scalardb/core.clj
@@ -18,17 +18,31 @@
 
 (def ^:const RETRIES 8)
 (def ^:const RETRIES_FOR_RECONNECTION 3)
+(def ^:private ^:const NUM_FAILURES_FOR_RECONNECTION 1000)
 
-(def ^:const COORDINATOR "coordinator")
-(def ^:const STATE_TABLE "state")
+(def ^:private ^:const COORDINATOR "coordinator")
+(def ^:private ^:const STATE_TABLE "state")
 (def ^:const VERSION "tx_version")
 
 (defn exponential-backoff
   [r]
   (Thread/sleep (reduce * 1000 (repeat r 2))))
 
-(defn create-coordinator-table!
-  [session test]
+(defn create-transfer-table
+  [test session {:keys [keyspace table schema]}]
+  (alia/execute session (create-keyspace keyspace
+                                         (if-exists false)
+                                         (with {:replication {"class"              "SimpleStrategy"
+                                                              "replication_factor" (:rf test)}})))
+  (alia/execute session (use-keyspace keyspace))
+  (alia/execute session (create-table table
+                                      (if-exists false)
+                                      (column-definitions schema)))
+  (alia/execute session (alter-table table
+                                     (with {:compaction {:class :SizeTieredCompactionStrategy}}))))
+
+(defn create-coordinator-table
+  [test session]
   (alia/execute session (create-keyspace COORDINATOR
                                          (if-exists false)
                                          (with {:replication {"class"              "SimpleStrategy"
@@ -40,6 +54,15 @@
                                                            :tx_state      :int
                                                            :tx_created_at :bigint
                                                            :primary-key   [:tx_id]}))))
+
+(defn setup-transaction-tables
+  [test schemata]
+  (let [session (alia/connect
+                 (alia/cluster {:contact-points (->> test :nodes (map name))}))]
+    (doseq [schema schemata]
+      (create-transfer-table test session schema))
+    (create-coordinator-table test session)
+    (alia/shutdown session)))
 
 (defn- create-properties
   [nodes]
@@ -126,35 +149,60 @@
         (when-not (nil? (:transaction test))
           (recur (dec tries)))))))
 
+(defn check-connection!
+  [test]
+  (when (nil? @(:transaction test))
+    (prepare-transaction-service! test)))
+
+(defn try-reconnection!
+  [test]
+  (when (= (swap! (:failures test) inc) NUM_FAILURES_FOR_RECONNECTION)
+    (prepare-transaction-service! test)
+    (reset! (:failures test) 0)))
+
 (defn start-transaction
   [test]
   (some-> test :transaction deref .start))
+
+(defmacro with-retry
+  [connect-fn test & body]
+  "If the result of the body is nil, it retries it"
+  `(loop [tries# RETRIES]
+     (when (< tries# RETRIES)
+       (exponential-backoff (- RETRIES tries#)))
+     (when (zero? (mod tries# RETRIES_FOR_RECONNECTION))
+       (~connect-fn ~test))
+     (let [results# ~@body]
+       (if-not (nil? results#)
+         results#
+         (if (pos? tries#)
+           (recur (dec tries#))
+           (throw (ex-info "Failed to read records"
+                           {:cause "Failed to read records"})))))))
 
 (defn- is-committed-state?
   "Return true if the status is COMMITTED. Return nil if the read fails."
   [coordinator id]
   (try
-      (let [state (.getState coordinator id)]
-        (and (.isPresent state)
-             (-> state .get .getState (.equals TransactionState/COMMITTED))))
-      (catch Exception e nil)))
+    (let [state (.getState coordinator id)]
+      (and (.isPresent state)
+           (-> state .get .getState (.equals TransactionState/COMMITTED))))
+    (catch Exception e nil)))
 
 (defn check-transaction-states
-  "Return the number of COMMITTED states by checking the coordinator. Return nil if it fails."
+  "Return the number of COMMITTED states by checking the coordinator."
   [test ids]
-  (loop [tries RETRIES]
-    (if-not (pos? tries)
-      nil
-      (do
-        (when (< tries RETRIES)
-          (exponential-backoff (- RETRIES tries)))
-        (when (zero? (mod tries RETRIES_FOR_RECONNECTION))
-          (prepare-storage-service! test)) ; reconnection
-        (let [coordinator (Coordinator. (deref (:storage test)))
-              committed   (map (partial is-committed-state? coordinator) ids)]
-          (if-not (some nil? committed)
-            (->> committed (filter true?) count)
-            (recur (dec tries))))))))
+  (if (seq ids)
+    (do
+      (when (nil? @(:storage test))
+        (prepare-storage-service! test))
+      (with-retry prepare-storage-service! test
+        (let [coordinator (Coordinator. @(:storage test))
+              committed (map (partial is-committed-state? coordinator) ids)]
+          (if (some nil? committed)
+            nil
+            (count (filter true? committed))))))
+    0))
 
 (defn scalardb-test
   [name opts]
--- scalardb/src/scalardb/transfer.clj
+++ scalardb/src/scalardb/transfer.clj
@@ -1,7 +1,5 @@
 (ns scalardb.transfer
-  (:require [cassandra
-             [core :as c]
-             [conductors :as conductors]]
+  (:require [cassandra.conductors :as conductors]
             [clojure.tools.logging :refer [debug info warn]]
             [clojure.core.reducers :as r]
             [jepsen
@@ -9,9 +7,6 @@
              [checker :as checker]
              [generator :as gen]]
             [knossos.op :as op]
-            [qbits.alia :as alia]
-            [qbits.hayt.dsl.clause :refer :all]
-            [qbits.hayt.dsl.statement :refer :all]
             [scalardb.core :as scalar])
   (:import (com.scalar.database.api Consistency
                                     DistributedStorage
@@ -26,45 +21,32 @@
                                                       CrudException
                                                       UnknownTransactionStatusException)))
 
-(def ^:const KEYSPACE "jepsen")
-(def ^:const TABLE "transfer")
-(def ^:const ACCOUNT_ID "account_id")
-(def ^:const BALANCE "balance")
+(def ^:private ^:const KEYSPACE "jepsen")
+(def ^:private ^:const TABLE "transfer")
+(def ^:private ^:const ACCOUNT_ID "account_id")
+(def ^:private ^:const BALANCE "balance")
 
-(def ^:const INITIAL_BALANCE 10000)
-(def ^:const NUM_ACCOUNTS 10)
-(def ^:const total-balance (* NUM_ACCOUNTS INITIAL_BALANCE))
+(def ^:private ^:const INITIAL_BALANCE 10000)
+(def ^:private ^:const NUM_ACCOUNTS 10)
+(def ^:private ^:const TOTAL_BALANCE (* NUM_ACCOUNTS INITIAL_BALANCE))
 
-(def ^:const NUM_FAILURES_FOR_RECONNECTION 1000)
+(def ^:private ^:const SCHEMA {:account_id             :int
+                               :balance                :int
+                               :tx_id                  :text
+                               :tx_version             :int
+                               :tx_state               :int
+                               :tx_prepared_at         :bigint
+                               :tx_committed_at        :bigint
+                               :before_account_id      :int
+                               :before_balance         :int
+                               :before_tx_id           :text
+                               :before_tx_version      :int
+                               :before_tx_state        :int
+                               :before_tx_prepared_at  :bigint
+                               :before_tx_committed_at :bigint
+                               :primary-key            [:account_id]})
 
-(defn- create-transfer-table!
-  [session test]
-  (alia/execute session (create-keyspace KEYSPACE
-                                         (if-exists false)
-                                         (with {:replication {"class"              "SimpleStrategy"
-                                                              "replication_factor" (:rf test)}})))
-  (alia/execute session (use-keyspace KEYSPACE))
-  (alia/execute session (create-table TABLE
-                                      (if-not-exists)
-                                      (column-definitions {:account_id             :int
-                                                           :balance                :int
-                                                           :tx_id                  :text
-                                                           :tx_version             :int
-                                                           :tx_state               :int
-                                                           :tx_prepared_at         :bigint
-                                                           :tx_committed_at        :bigint
-                                                           :before_account_id      :int
-                                                           :before_balance         :int
-                                                           :before_tx_id           :text
-                                                           :before_tx_version      :int
-                                                           :before_tx_state        :int
-                                                           :before_tx_prepared_at  :bigint
-                                                           :before_tx_committed_at :bigint
-                                                           :primary-key            [:account_id]})))
-  (alia/execute session (alter-table TABLE
-                                     (with {:compaction {:class :SizeTieredCompactionStrategy}}))))
-
-(defn prepare-get
+(defn- prepare-get
   [id]
   (-> (Key. [(IntValue. ACCOUNT_ID id)])
       (Get.)
@@ -72,7 +54,7 @@
       (.forTable TABLE)
       (.withConsistency Consistency/LINEARIZABLE)))
 
-(defn prepare-put
+(defn- prepare-put
   [id v]
   (-> (Key. [(IntValue. ACCOUNT_ID id)])
       (Put.)
@@ -81,7 +63,7 @@
       (.withValue v)
       (.withConsistency Consistency/LINEARIZABLE)))
 
-(defn populate-accounts
+(defn- populate-accounts
   "Insert initial records with transaction.
   This method assumes that n is small (< 100)"
   [test n balance]
@@ -89,20 +71,37 @@
     (try
       (dotimes [i n]
         (->>
-          (prepare-put i (IntValue. BALANCE balance))
-          (.put tx)))
+         (prepare-put i (IntValue. BALANCE balance))
+         (.put tx)))
       (.commit tx)
       (catch Exception e
         (throw (RuntimeException. (.getMessage e)))))))
 
-(defn calc-new-val
-  "Calculate the new value from the result of transaction.get()"
-  [r amount]
-  (IntValue. BALANCE
-             (-> r .get (.getValue BALANCE) .get .get (+ amount))))
+(defn- get-balance
+  "Get a balance from a result"
+  [^Result r]
+  (-> r .get (.getValue BALANCE) .get .get))
 
-(defn tx-transfer
-  [tx from to amount]
+(defn- get-version
+  "Get a version from a result"
+  [^Result r]
+  (-> r .get (.getValue scalar/VERSION) .get .get))
+
+(defn- get-balances
+  [results]
+  (mapv get-balance results))
+
+(defn- get-versions
+  [results]
+  (mapv get-version results))
+
+(defn- calc-new-val
+  "Calculate the new value from the result of transaction.get()"
+  [^Result r ^long amount]
+  (IntValue. BALANCE (-> r get-balance (+ amount))))
+
+(defn- tx-transfer
+  [tx {:keys [from to amount]}]
   (let [fromResult (.get tx (prepare-get from))
         toResult (.get tx (prepare-get to))]
     (->> (calc-new-val fromResult (- amount))
@@ -113,58 +112,20 @@
          (.put tx))
     (.commit tx)))
 
-(defn read-record
+(defn- read-record
   "Read a record with a transaction. If read fails, this function returns nil."
   [tx i]
   (try
     (.get tx (prepare-get i))
     (catch CrudException e nil)))
 
-(defn read-all-records
-  "Read records from 0 .. (n - 1)"
+(defn- read-all-with-retry
   [test n]
-  (let [tx (scalar/start-transaction test)]
-    (map #(read-record tx %) (range n))))
-
-(defn read-all-with-retry
-  "Read records from 0 .. (n - 1) and retry if needed"
-  [test n]
-  (when (nil? (deref (:transaction test)))
-    (scalar/prepare-transaction-service! test))
-  (loop [tries scalar/RETRIES]
-    (when (< tries scalar/RETRIES)
-      (scalar/exponential-backoff (- scalar/RETRIES tries)))
-    (when (zero? (mod tries scalar/RETRIES_FOR_RECONNECTION))
-      (scalar/prepare-transaction-service! test))
-    (let [results (read-all-records test n)]
-      (if (empty? (filter nil? results))
-        results
-        (if (pos? tries)
-          (recur (dec tries))
-          (throw (ex-info "Failed to read all records"
-                          {:cause "Failed to read all records"})))))))
-
-(defn get-balance
-  "Get a balance from a result"
-  [r]
-  (-> r .get (.getValue BALANCE) .get .get))
-
-(defn get-version
-  "Get a version from a result"
-  [r]
-  (-> r .get (.getValue scalar/VERSION) .get .get))
-
-(defn get-balances-and-versions
-  "Read all records with a transaction. Return only balances and versions."
-  [test n]
-  (let [results (read-all-with-retry test n)]
-    (map #(assoc {} :balance (get-balance %) :version (get-version %)) results)))
-
-(defn- try-reconnection!
-  [test]
-  (when (= (swap! (:failures test) inc) NUM_FAILURES_FOR_RECONNECTION)
-    (scalar/prepare-transaction-service! test)
-    (reset! (:failures test) 0)))
+  (scalar/check-connection! test)
+  (scalar/with-retry scalar/prepare-transaction-service! test
+    (let [tx (scalar/start-transaction test)
+          results (map #(read-record tx %) (range n))]
+      (if (some nil? results) nil results))))
 
 (defrecord TransferClient [initialized? n initial-balance]
   client/Client
@@ -174,11 +135,9 @@
   (setup! [_ test]
     (locking initialized?
       (when (compare-and-set! initialized? false true)
-        (let [session (alia/connect
-                        (alia/cluster {:contact-points (->> test :nodes (map name))}))]
-          (create-transfer-table! session test)
-          (scalar/create-coordinator-table! session test)
-          (alia/shutdown session))
+        (scalar/setup-transaction-tables test [{:keyspace KEYSPACE
+                                                :table TABLE
+                                                :schema SCHEMA}])
         (scalar/prepare-storage-service! test)
         (scalar/prepare-transaction-service! test)
         (populate-accounts test n initial-balance))))
@@ -187,33 +146,32 @@
     (case (:f op)
       :transfer (if-let [tx (scalar/start-transaction test)]
                   (try
-                    (tx-transfer tx (-> op :value :from) (-> op :value :to) (-> op :value :amount))
+                    (tx-transfer tx (:value op))
                     (assoc op :type :ok)
                     (catch UnknownTransactionStatusException e
                       (swap! (:unknown-tx test) conj (.getId tx))
                       (assoc op :type :fail :error {:unknown-tx-status (.getId tx)}))
                     (catch Exception e
-                      (try-reconnection! test)
+                      (scalar/try-reconnection! test)
                       (assoc op :type :fail :error (.getMessage e))))
                   (do
-                    (try-reconnection! test)
+                    (scalar/try-reconnection! test)
                     (assoc op :type :fail :error "Skipped due to no connection")))
-      :get-all (if-let [results (get-balances-and-versions test (:num op))]
-                 (assoc op :type :ok :value results)
+      :get-all (if-let [results (read-all-with-retry test (:num op))]
+                 (assoc op :type :ok :value {:balance (get-balances results)
+                                             :version (get-versions results)})
                  (assoc op :type :fail :error "Failed to get balances"))
-      :check-tx (let [unknown (:unknown-tx test)]
-                  (if-let [num-committed (if (empty? @unknown)
-                                           0
-                                           (scalar/check-transaction-states test @unknown))]
-                    (assoc op :type :ok :value num-committed)
-                    (assoc op :type :fail :error "Failed to check status")))))
+      :check-tx (if-let [num-committed (scalar/check-transaction-states test
+                                                                        @(:unknown-tx test))]
+                  (assoc op :type :ok, :value num-committed)
+                  (assoc op :type :fail, :error "Failed to check status"))))
 
   (close! [_ _])
 
   (teardown! [_ test]
     (scalar/close-all! test)))
 
-(defn transfer
+(defn- transfer
   [test _]
   (let [n (-> test :model :num)]
     {:type  :invoke
@@ -227,18 +185,18 @@
                              (-> op :value :to)))
               transfer))
 
-(defn get-all
+(defn- get-all
   [test _]
   {:type :invoke
    :f    :get-all
    :num  (-> test :model :num)})
 
-(defn check-tx
+(defn- check-tx
   [test _]
   {:type :invoke
    :f    :check-tx})
 
-(defn consistency-checker
+(defn- consistency-checker
   []
   (reify checker/Checker
     (check [this test history opts]
@@ -249,15 +207,13 @@
                              (into [])
                              last
                              :value)
-            actual-balance (->> read-result
-                                (map :balance)
+            actual-balance (->> (:balance read-result)
                                 (reduce +))
-            bad-balance (if-not (= actual-balance total-balance)
+            bad-balance (if-not (= actual-balance TOTAL_BALANCE)
                           {:type     :wrong-balance
-                           :expected total-balance
+                           :expected TOTAL_BALANCE
                            :actual   actual-balance})
-            actual-version (->> read-result
-                                (map :version)
+            actual-version (->> (:version read-result)
                                 (reduce +))
             checked-committed (->> history
                                    (r/filter #(= :check-tx (:f %)))
@@ -289,16 +245,17 @@
 (defn transfer-test
   [opts]
   (merge (scalar/scalardb-test (str "transfer-" (:suffix opts))
-                               {:client     (TransferClient. (atom false) NUM_ACCOUNTS INITIAL_BALANCE)
+                               {:client     (TransferClient. (atom false)
+                                                             NUM_ACCOUNTS
+                                                             INITIAL_BALANCE)
                                 :model      {:num NUM_ACCOUNTS}
                                 :unknown-tx (atom #{})
                                 :failures   (atom 0)
                                 :generator  (gen/phases
-                                              (->> [diff-transfer]
-                                                   (conductors/std-gen opts))
-                                              (conductors/terminate-nemesis opts)
-                                              (gen/clients (gen/once check-tx))
-                                              (gen/clients (gen/once get-all)))
-                                :checker    (checker/compose
-                                              {:details (consistency-checker)})})
+                                             (->> [diff-transfer]
+                                                  (conductors/std-gen opts))
+                                             (conductors/terminate-nemesis opts)
+                                             (gen/clients (gen/once check-tx))
+                                             (gen/clients (gen/once get-all)))
+                                :checker    (consistency-checker)})
          opts))
