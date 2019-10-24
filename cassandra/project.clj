0230be9980d0af8470da165ac61d35707f2689dd
yito88 <yuji@phact-columba.com>
2019-10-24 13:40:01 +0900

    update dependencies

--- cassandra/project.clj
+++ cassandra/project.clj
@@ -3,13 +3,12 @@
   :url "http://github.com/scalar-labs/jepsen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/java.jmx "0.3.1"]
                  [jepsen "0.1.13"]
                  [cc.qbits/alia "4.3.1"]
                  [cc.qbits/hayt "4.1.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
-                                  [tortue/spy "2.0.0"]]
-                   :plugins [[lein-cloverage "1.1.1"]]}}
+  :profiles {:dev {:dependencies [[tortue/spy "2.0.0"]]
+                   :plugins [[lein-cloverage "1.1.2"]]}}
   :main cassandra.runner
   :aot [cassandra.runner])
--- scalardl/project.clj
+++ scalardl/project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/scalar-labs/scalar-jepsen"
   :license {:name ""
             :url ""}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [jepsen "0.1.13"]
                  [cassandra "0.1.0-SNAPSHOT"]
                  [cc.qbits/alia "4.3.1"]
@@ -11,9 +11,8 @@
                  [com.scalar-labs/scalardl-client-sdk "1.2.0"
                   :exclusions [[org.slf4j/slf4j-log4j12]
                                [com.google.guava/guava]]]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]
-                                  [tortue/spy "2.0.0"]]
-                   :plugins [[lein-cloverage "1.1.1"]]}}
+  :profiles {:dev {:dependencies [[tortue/spy "2.0.0"]]
+                   :plugins [[lein-cloverage "1.1.2"]]}}
   :java-source-paths ["contract"]
   :main scalardl.runner
   :aot :all)
