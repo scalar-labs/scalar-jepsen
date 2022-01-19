357c9582be2aa2972a7dda83d6042df5f3aa2bbb
scalar-boney <boney.jacob@scalar-labs.com>
2022-01-19 15:07:15 +0900

    update client sdk and protobuf version

--- scalardl/project.clj
+++ scalardl/project.clj
@@ -10,12 +10,12 @@
                  [cc.qbits/hayt "4.1.0"]]
   :profiles {:dev {:dependencies [[tortue/spy "2.0.0"]]
                    :plugins [[lein-cloverage "1.1.2"]]}
-             :use-released {:dependencies [[com.scalar-labs/scalardl-java-client-sdk "3.0.1" :exclusions [org.slf4j/slf4j-log4j12]]]}
+             :use-released {:dependencies [[com.scalar-labs/scalardl-java-client-sdk "3.3.1" :exclusions [org.slf4j/slf4j-log4j12]]]}
              :use-jars {:dependencies [[org.bouncycastle/bcpkix-jdk15on "1.59"]
                                        [org.bouncycastle/bcprov-jdk15on "1.59"]
                                        [com.google.inject/guice "4.2.0"]
                                        [com.moandjiezana.toml/toml4j "0.7.2"]
-                                       [com.google.protobuf/protobuf-java-util "3.13.0"]
+                                       [com.google.protobuf/protobuf-java-util "3.19.0"]
                                        [com.scalar-labs/scalar-admin "1.0.0"
                                         :exclusions [org.slf4j/slf4j-log4j12]]]
                         :resource-paths ["resources/scalardl-java-client-sdk.jar"
