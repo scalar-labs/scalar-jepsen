bb69393dae7c84294d0c0e03d0dc7545cf813341
Hiroyuki Yamada <mogwaing@gmail.com>
2023-02-01 13:53:51 +0900

    Revert "Upgrade Jepsen to 0.3.1 (#93)" (#94)
    This reverts commit 4197e7f5f050e8af93147dd4e05873ce18144a60.

--- cassandra/project.clj
+++ cassandra/project.clj
@@ -3,12 +3,9 @@
   :url "http://github.com/scalar-labs/jepsen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/java.jmx "0.3.1"]
-                 [jepsen "0.3.1" :exclusions [net.java.dev.jna/jna
-                                              net.java.dev.jna/jna-platform]]
-                 [net.java.dev.jna/jna "5.11.0"]
-                 [net.java.dev.jna/jna-platform "5.11.0"]
+                 [jepsen "0.2.1"]
                  [cc.qbits/alia "4.3.6"]
                  [cc.qbits/hayt "4.1.0"]]
   :profiles {:dev {:dependencies [[tortue/spy "2.0.0"]]
