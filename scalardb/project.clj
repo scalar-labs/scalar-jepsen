95feb42dd603e8eef82ba1222e37ddb397b38621
Toshihiro Suzuki <brfrn169@gmail.com>
2025-01-14 14:18:46 +0900

    Exclude commons-lang3 lib from Cassandra dependency in ScalarDB and ScalarDL tests (#134)

--- scalardb/project.clj
+++ scalardb/project.clj
@@ -7,7 +7,7 @@
                  [net.java.dev.jna/jna "5.11.0"]
                  [net.java.dev.jna/jna-platform "5.11.0"]
                  [org.slf4j/slf4j-jdk14 "2.0.6"]
-                 [cassandra "0.1.0-SNAPSHOT"]
+                 [cassandra "0.1.0-SNAPSHOT" :exclusions [org.apache.commons/commons-lang3]]
                  [cheshire "5.12.0"]
                  [com.scalar-labs/scalardb-schema-loader "4.0.0-SNAPSHOT"]]
   :repositories {"sonartype" "https://oss.sonatype.org/content/repositories/snapshots/"}
@@ -21,7 +21,7 @@
                                                          io.grpc/grpc-core
                                                          com.scalar-labs/scalardb-rpc]]]}
              :use-jars {:dependencies [[com.google.guava/guava "31.1-jre"]
-                                       [org.apache.commons/commons-text "1.10.0"]]
+                                       [org.apache.commons/commons-text "1.13.0"]]
                         :resource-paths ["resources/scalardb.jar"]}
              :default [:base :system :user :provided :dev :use-released]}
   :jvm-opts ["-Djava.awt.headless=true"]
