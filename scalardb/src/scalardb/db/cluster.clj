22921c54ee72b3701a104fee483fa9d7e4375001
yito88 <yuji@phact-columba.com>
2025-01-30 23:37:45 +0100

    cluster test

---
+++ scalardb/src/scalardb/db/cluster.clj
@@ -0,0 +1,178 @@
+(ns scalardb.db.cluster
+  (:require [clojure.string :as str]
+            [clojure.tools.logging :refer [info]]
+            [jepsen
+             [control :as c]
+             [db :as db]]))
+
+(def ^:private ^:const DEFAULT_VERSION "3.14.0")
+(def ^:private ^:const CLUSTER_VALUES_YAML "scalardb-cluster-custom-values.yaml")
+
+(def ^:private ^:const TIMEOUT_SEC 600)
+(def ^:private ^:const INTERVAL_SEC 10)
+
+(defn- install!
+  "Install prerequisites. You should already have installed minikube, kubectl and helm."
+  []
+  ;; postgre
+  (c/exec :helm :repo :add "bitnami" "https://charts.bitnami.com/bitnami")
+  ;; ScalarDB cluster
+  (c/exec :helm :repo :add
+          "scalar-labs" "https://scalar-labs.github.io/helm-charts"))
+
+(defn- configure!
+  [test]
+  (binding [c/*dir* (System/getProperty "user.dir")]
+    (try
+      (c/exec :kubectl :delete :secret "scalardb-ghcr-secret")
+      ;; ignore the failure when the secret doesn't exist
+      (catch Exception _))
+    (c/exec :kubectl :create :secret :docker-registry "scalardb-ghcr-secret"
+            "--docker-server=ghcr.io"
+            (str "--docker-username=" (:docker-username test))
+            (str "--docker-password=" (:docker-access-token test)))
+    (try
+      (c/exec :kubectl :delete :secret "scalardb-credentials-secret")
+      ;; ignore the failure when the secret doesn't exist
+      (catch Exception _))
+    (c/exec :kubectl :create :secret :generic "scalardb-credentials-secret"
+            "--from-literal=SCALAR_DB_CLUSTER_POSTGRES_USERNAME=postgres"
+            "--from-literal=SCALAR_DB_CLUSTER_POSTGRES_PASSWORD=postgres"
+            :-n "default")))
+
+(defn- start!
+  []
+  ;; postgre
+  (c/exec
+   :helm :install "postgresql-scalardb-cluster" "bitnami/postgresql"
+   :--set "auth.postgresPassword=postgres"
+   :--set "primary.persistence.enabled=false"
+   ;; Need an external IP for storage APIs
+   :--set "service.type=LoadBalancer"
+   :--set "primary.service.type=LoadBalancer")
+
+  ;; ScalarDB cluster
+  (let [chart-version (->> (c/exec :helm :search
+                                   :repo "scalar-labs/scalardb-cluster" :-l)
+                           str/split-lines
+                           (filter #(str/includes? % DEFAULT_VERSION))
+                           (map #(nth (str/split % #"\s+") 1))
+                           (sort #(compare %2 %1))
+                           first)]
+    (info "helm chart version:" chart-version)
+    (binding [c/*dir* (System/getProperty "user.dir")]
+      (c/exec :helm :install "scalardb-cluster" "scalar-labs/scalardb-cluster"
+              :-f CLUSTER_VALUES_YAML
+              :--version chart-version
+              :-n "default"))))
+
+(defn- wipe!
+  []
+  (try
+    (info "wiping old logs...")
+    (binding [c/*dir* (System/getProperty "user.dir")]
+      (some->> (-> (c/exec :ls) (str/split #"\s+"))
+           (filter #(re-matches #"scalardb-cluster-node-.*\.log" %))
+           seq
+           (apply c/exec :rm :-f)))
+    (info "wiping the pods...")
+    (c/exec :helm :uninstall :scalardb-cluster :postgresql-scalardb-cluster)
+    (catch Exception _ nil)))
+
+(defn- get-logs
+  [_test]
+  (binding [c/*dir* (System/getProperty "user.dir")]
+    (let [pods (->> (c/exec :kubectl :get :pod)
+                    str/split-lines
+                    (filter #(str/includes? % "scalardb-cluster-node-"))
+                    (filter #(str/includes? % "Running"))
+                    (map #(first (str/split % #"\s+"))))
+          logs (map #(str c/*dir* \/ % ".log") pods)]
+      (mapv #(spit %1 (c/exec :kubectl :logs %2)) logs pods)
+      logs)))
+
+(defn get-load-balancer-ip
+  "Get the IP of the load balancer"
+  []
+  (->> (c/exec :kubectl :get :svc)
+       str/split-lines
+       (filter #(str/includes? % "scalardb-cluster-envoy"))
+       (filter #(str/includes? % "LoadBalancer"))
+       (map #(nth (str/split % #"\s+") 3))
+       first))
+
+(defn get-postgres-ip
+  "Get the IP of the load balancer"
+  []
+  (->> (c/exec :kubectl :get :svc)
+       str/split-lines
+       (filter #(str/includes? % "postgresql-scalardb-cluster"))
+       (filter #(str/includes? % "LoadBalancer"))
+       (map #(nth (str/split % #"\s+") 3))
+       first))
+
+(defn running-pods?
+  "Check a live node."
+  [_test]
+  (let [node (-> test :nodes first)]
+    (->> (c/on node (c/exec :kubectl :get :pod))
+         str/split-lines
+         (filter #(str/includes? % "scalardb-cluster-node-"))
+         (filter #(str/includes? % "Running"))
+         count
+         ;; TODO: check the number of pods
+         (= 3))))
+
+(defn wait-for-recovery
+  "Wait for the node bootstrapping."
+  ([test]
+   (wait-for-recovery TIMEOUT_SEC INTERVAL_SEC test))
+  ([timeout-sec interval-sec test]
+   (when (not (running-pods? test))
+     (Thread/sleep (* interval-sec 1000))
+     (if (>= timeout-sec interval-sec)
+       (wait-for-recovery (- timeout-sec interval-sec) interval-sec test)
+       (throw (ex-info "Timed out waiting for pods"
+                       {:causes "Some pod couldn't start"}))))))
+
+(defn db
+  "Setup ScalarDB cluster."
+  []
+  (reify
+    db/DB
+    (setup! [_ test _]
+      (when-not (:leave-db-running? test)
+        (wipe!))
+      (install!)
+      (configure! test)
+      (start!)
+      ;; wait for the pods
+      (wait-for-recovery test))
+
+    (teardown! [_ test _]
+      (when-not (:leave-db-running? test)
+        (wipe!)))
+
+    db/Primary
+    (primaries [_ test] (:nodes test))
+    (setup-primary! [_ _ _])
+
+    db/Pause
+    (pause! [_ _ _]
+      ;; TODO
+      nil)
+    (resume! [_ _ _]
+      ;; TODO
+      nil)
+
+    db/Kill
+    (start! [_ _ _]
+      ;; TODO
+      nil)
+    (kill! [_ _ _]
+      ;; TODO
+      nil)
+
+    db/LogFiles
+    (log-files [_ test _]
+      (get-logs test))))
