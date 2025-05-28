52048dd1b49cdaf8b6c55c41101074b0a51be765
yito88 <yito88@protonmail.com>
2025-05-28 23:03:24 +0200

    revert sdk

--- scalardb/project.clj
+++ scalardb/project.clj
@@ -22,7 +22,7 @@
                                                          com.azure/azure-cosmos
                                                          io.grpc/grpc-core
                                                          com.scalar-labs/scalardb-rpc]]]}
-             :cluster {:dependencies [[com.scalar-labs/scalardb-cluster-java-client-sdk "4.0.0-SNAPSHOT"
+             :cluster {:dependencies [[com.scalar-labs/scalardb-cluster-java-client-sdk "3.15.3"
                                        ;; avoid the netty dependency issue
                                        :exclusions [software.amazon.awssdk/*
                                                     com.oracle.database.jdbc/ojdbc8-production
