e0863faef73debcb1e91eeb01d9f8d05ec1979b6
yito88 <yito88@protonmail.com>
2026-01-28 21:36:42 +0100

    longer timeout

--- scalardb/src/scalardb/db/cluster.clj
+++ scalardb/src/scalardb/db/cluster.clj
@@ -227,7 +227,7 @@
                         "--for=condition=Ready"
                         "pod"
                         :-l (str "app=" ORACLE_NAME)
-                        "--timeout=120s"))
+                        "--timeout=600s"))
     (throw-unsupported-db-error db-type))
 
   ;; ScalarDB Cluster
