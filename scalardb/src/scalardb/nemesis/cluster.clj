5b1ff51d135c3d091200d6a289163cca0d1e05b8
yito88 <yito88@protonmail.com>
2026-01-30 22:44:39 +0100

    fix

--- scalardb/src/scalardb/nemesis/cluster.clj
+++ scalardb/src/scalardb/nemesis/cluster.clj
@@ -146,9 +146,9 @@
                   :-n "chaos-mesh"
                   :-p "{\"metadata\":{\"finalizers\":[]}}"
                   :--type "merge"
-                  ;; Need the deletion command with the pipe
+                  ;; Need the deletion command immediately
                   ;; because the finalizers could be reverted
-                  c/|
+                  c/&&
                   :kubectl :delete "networkchaos" action :-n "chaos-mesh"))
         (.delete file)))))
 
