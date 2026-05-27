b92a35c1060fa3d19234a7e35747ee47c60c2dbe
yito88 <yito88@protonmail.com>
2026-05-27 20:25:20 +0200

    fix dl

--- scalardl/test/scalardl/cassandra_test.clj
+++ scalardl/test/scalardl/cassandra_test.clj
@@ -8,27 +8,23 @@
 (def TX_ABORTED 4)
 
 (deftest committed-test
-  (with-redefs [alia/cluster (spy/stub "cluster")
-                alia/connect (spy/stub "session")
+  (with-redefs [alia/session (spy/stub "session")
                 alia/execute (spy/stub [{:tx_state TX_COMMITTED}])]
-    (is (true? (cassandra/committed? "txid" {:cass-nodes "localhost"})))))
+    (is (true? (cassandra/committed? "txid" {:cass-nodes ["localhost"]})))))
 
 (deftest committed-when-aborted-test
-  (with-redefs [alia/cluster (spy/stub "cluster")
-                alia/connect (spy/stub "session")
+  (with-redefs [alia/session (spy/stub "session")
                 alia/execute (spy/stub [{:tx_state TX_ABORTED}])]
-    (is (false? (cassandra/committed? "txid" {:cass-nodes "localhost"})))))
+    (is (false? (cassandra/committed? "txid" {:cass-nodes ["localhost"]})))))
 
 (deftest committed-when-no-state-test
-  (with-redefs [alia/cluster (spy/stub "cluster")
-                alia/connect (spy/stub "session")
+  (with-redefs [alia/session (spy/stub "session")
                 alia/execute (spy/stub [])]
-    (is (false? (cassandra/committed? "txid" {:cass-nodes "localhost"})))))
+    (is (false? (cassandra/committed? "txid" {:cass-nodes ["localhost"]})))))
 
 (deftest committed-when-read-fail-test
-  (with-redefs [alia/cluster (spy/stub "cluster")
-                alia/connect (spy/stub "session")
+  (with-redefs [alia/session (spy/stub "session")
                 alia/execute (spy/mock (fn [_ _ _]
                                          (throw (ex-info "fail" {}))))]
     (is (thrown? clojure.lang.ExceptionInfo
-                 (cassandra/committed? "txid" {:cass-nodes "localhost"})))))
+                 (cassandra/committed? "txid" {:cass-nodes ["localhost"]})))))
